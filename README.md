1374c9a3d4c29d202af37d0b7e819fa690fc6948
Falcon <88769526+falconvn2006@users.noreply.github.com>
2024-04-29 00:46:45 +0000

    Changes some basic stuff

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # portfolio-game
-A 2D game of my portfolio. This was inspired by JSLegendDev. Check out his tutorials :D <br>
+A 2D game of my portfolio. Made in Javascript using Vite and Kaboom<br>
+This was inspired by [JSLegendDev](https://github.com/JSLegendDev/). Check out his tutorials :D <br>
 [Click here](https://youtu.be/wy_fSStEgMs?si=nBsSowCZuL0X2FCC) to watch his video on how to make this portfolio.
--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,18 @@
 export const scaleFactor = 4
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-        I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-        Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-        <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    pc: `This is my PC. I like using C++ and Python these days.
+        But sometimes I made a couple of games in Unity using C#. I might learn more languages in the future. Anyway regardless of the language, I just like programming.
+        Here is my <a href="https://github.com/falconvn2006" target="_blank">Github</a>!`,
+    "cs-degree": `This is my HSG Tin Hoc Certification (3rd place). I hung it on the wall because I'm proud of it. It was a simple contest from high school. But I was still a noob back then`,
+    "sofa-table": `That's my sofa. I like to relax here, watch YouTube and play video games!`,
+    tv: `That's my TV. I've been watching tech and math related youtubers a lot recently like :
+        <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>, <a href="https://www.youtube.com/@SebastianLague" target="_blank">Sebastian Lague</a> and
+    <a href="https://www.youtube.com/@3blue1brown" target="_blank">3Blue1Brown</a>!`,
+    bed: `This where I sleep. I usually have some books right next to me, then when I can't sleep I would pick one to read.`,
+    resume: `This is my desk. I often draw when I have free time or just too bored. This is where I often self-study a bunch of topics: Quantum Mechanics, Computer Science,...`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+    I learned how to make this from JSLegendDev. Check him out <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `There are a lot of programming/computer books on my shelves. I also have some academic related books. I also learn Japanese and French. Not good at them yet but still learning`,
+    exit: `If you want to exit Falcon's portfolio, just close the tab.`,
 };
